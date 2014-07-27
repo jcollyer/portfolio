778adbec594f853da912dfeab0fdec812a453bb8
jcollyer <collyerdesign@gmail.com>
2014-07-27 10:51:32 -0700

    REFACTOR - clean up - remove old multi-page code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,28 +14,13 @@ angular.module('portfolio', [
     })
     .state('jobs', {
       url: '/jobs',
-      templateUrl: 'partials/jobs.html',
-      controller: 'JobListController'
-    }).state('viewJob', { //state for showing single job
-      url: '/jobs/:id/view',
-      templateUrl: 'partials/job-view.html',
-      controller: 'JobViewController'
-    }).state('newJob', { //state for adding a new job
-      url: '/jobs/new',
-      templateUrl: 'partials/job-add.html',
-      controller: 'JobCreateController'
-    }).state('editJob', { //state for updating a job
-      url: '/jobs/:id/edit',
-      templateUrl: 'partials/job-edit.html',
-      controller: 'JobEditController'
+      templateUrl: 'jobs.html',
+      controller: 'JobsController'
     });
     $urlRouterProvider.otherwise('/');
 })
 
-.controller('JobListController', function($scope, $state, Job, popupService){
-  // $scope.jobs = Job.get().$promise.then(function(res){
-  //   $scope.jobs = res.jobs;
-  // });
+.controller('JobsController', function($scope, $state, Job, popupService){
 
   $scope.jobs = Job.query();
   $scope.job = new Job();
@@ -51,78 +36,24 @@ angular.module('portfolio', [
     $scope.editing = false;
     $scope.job = new Job();
   };
+
   $scope.newJob    = function() {
     $scope.job = new Job();
     $scope.editing = false;
   };
 
   $scope.activePost = function(job) {
+    debugger;
     $scope.job = job;
     $scope.editing = true;
-  }
+  };
+
   $scope.delete = function(job) {
     if (popupService.showPopup('Really delete this?')) {
       Job.delete(job);
       _.remove($scope.job, job);
     }
   };
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-.controller('JobViewController', function($scope, $stateParams, Job) {
-  $scope.job = Job.get().$promise.then(function(res){
-    thisId = $stateParams.id -1;
-    $scope.job = res.jobs[thisId];
-  });
-
-
-  // $scope.job=Job.get({id:$stateParams.id});
-})
-
-.controller('JobCreateController', function($scope, Job, $state) {
-  $scope.job = new Job();
-
-  $scope.addJob = function() {
-    $scope.job.$save().then(function() {
-      $state.go('jobs');
-    });
-  };
-})
-
-.controller('JobEditController', function($scope, $state, $stateParams, Job) {
-  $scope.loadJob = function(){
-    $scope.job = Job.get({id:$stateParams.id});
-    // debugger;
-
-  };
-
-  $scope.loadJob();
-
-  // $scope.job = Job.get().$promise.then(function(res){
-  //   thisId = $stateParams.id -1;
-  //   $scope.job = res.jobs[thisId];
-  // });
-
-  $scope.updateJob = function(job) {
-    // debugger;
-    job.$update().then(function() {
-      // debugger;
-      $state.go('jobs');
-    });
-  };
-
 
 })
 ;
--- app/assets/javascripts/models/job.js
+++ app/assets/javascripts/models/job.js
@@ -1,7 +1,6 @@
 angular.module('models.job', [])
 .factory('Job', function($resource) {
   var Job = $resource('/api/v1/jobs/:id', {id:'@id'}, {
-    get: { method: 'GET', isArray: false},
     update: { method: 'PUT' },
     delete_job: { method: 'DELETE' }
   });
